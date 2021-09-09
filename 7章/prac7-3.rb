4f525910ac4992f346db56230c39f0b72c42edae
suzuya0 <sakura240613@gmail.com>
2021-09-09 23:56:45 +0900

    [7章途中2]

--- 7章/prac7-3.rb
+++ 7章/prac7-3.rb
@@ -19,4 +19,17 @@ def dice
   end
 end
 
- dice
+
+def dice_core
+  [1,2,3,4,5,6].sample
+end
+
+def dice
+  result = dice_core
+  return result unless result == 1
+  puts "もう一回"
+  dice_core
+end
+
+puts dice
+
